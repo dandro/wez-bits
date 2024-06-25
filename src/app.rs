a90b217d67e35bc40c2c6dec994eecab81332d4c
Daniel Martinez <dandro.dsign@gmail.com>
2024-06-25 21:45:13 +1000

    Improve output
    Separate app from main
    Spawn child process and wait for it to finish while collecting the
    output and spitting that out to stdout

---
+++ src/app.rs
@@ -0,0 +1,95 @@
+use std::process::{Command, ExitStatus, Stdio};
+
+use clap::{Parser, ValueEnum};
+use serde::Deserialize;
+
+#[derive(Clone, ValueEnum)]
+enum ProjectileIntegration {
+    Build,
+    Format,
+    Run,
+    Test,
+}
+
+/// Helix Projectile - Project Scoped Interactivity
+#[derive(Parser)]
+struct HelixProjectile {
+    /// Integration Feature
+    #[arg(value_enum, short, long)]
+    integration: ProjectileIntegration,
+}
+
+#[derive(Deserialize)]
+struct ProjectileCommand {
+    program: String,
+    args: Vec<String>,
+}
+
+#[derive(Deserialize)]
+struct ProjectileConfig {
+    build: Option<ProjectileCommand>,
+    format: Option<ProjectileCommand>,
+    run: Option<ProjectileCommand>,
+    test: Option<ProjectileCommand>,
+}
+
+pub enum AppErr {
+    ProjectConfig(String),
+    FeatureNotConfigured(String),
+    CommandFailed(String),
+}
+
+impl ToString for AppErr {
+    fn to_string(&self) -> String {
+        let msg = match &self {
+            AppErr::ProjectConfig(msg) => format!("[kind] ProjectConfig [msg]: {} ", msg),
+            AppErr::FeatureNotConfigured(msg) => {
+                format!("[kind] FeatureNotConfigured [msg]: {} ", msg)
+            }
+            AppErr::CommandFailed(msg) => format!("[kind] CommandFailed [msg]: {} ", msg),
+        };
+        format!("[Helix Projectile Error] {} ", msg)
+    }
+}
+
+fn exec(cmd: ProjectileCommand) -> Result<ExitStatus, AppErr> {
+    Command::new(cmd.program)
+        .args(cmd.args)
+        .stdout(Stdio::inherit())
+        .spawn()
+        .and_then(|child| child.wait_with_output().map(|output| output.status))
+        .map_err(|e| AppErr::CommandFailed(e.to_string()))
+}
+
+fn get_cmd(
+    helix_projectile: HelixProjectile,
+    project_config: ProjectileConfig,
+) -> Result<ProjectileCommand, AppErr> {
+    match helix_projectile.integration {
+        ProjectileIntegration::Build => project_config.build.ok_or(err_with("build")),
+        ProjectileIntegration::Format => project_config.format.ok_or(err_with("format")),
+        ProjectileIntegration::Run => project_config.run.ok_or(err_with("run")),
+        ProjectileIntegration::Test => project_config.test.ok_or(err_with("test")),
+    }
+}
+
+fn err_with(feature: &str) -> AppErr {
+    AppErr::FeatureNotConfigured(format!("Project {} feature not configured", feature))
+}
+
+fn load_config() -> Result<ProjectileConfig, AppErr> {
+    std::fs::read_to_string(".hx/hx-projectile.json")
+        .map_err(|e| AppErr::ProjectConfig(e.to_string()))
+        .and_then(|config| {
+            serde_json::from_str::<ProjectileConfig>(&config)
+                .map_err(|e| AppErr::ProjectConfig(e.to_string()))
+        })
+}
+
+pub fn run_app() -> Result<ExitStatus, AppErr> {
+    let project_config = load_config()?;
+    let helix_projectile = HelixProjectile::parse();
+    let cmd = get_cmd(helix_projectile, project_config)?;
+
+    exec(cmd)
+}
