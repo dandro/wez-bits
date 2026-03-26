b2684e2d4e9e4890b320609c0d1de39d2c0b5012
Daniel Martinez <danmartinez@canva.com>
2026-03-26 17:24:50 +1100

    Only open a pane if it does not exists

--- src/adapters/terminal_adapter.rs
+++ src/adapters/terminal_adapter.rs
@@ -1,6 +1,6 @@
 use std::process::{Command, ExitStatus, Stdio};
 
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use log::info;
 
 use crate::domain::models::{Direction, TaskClose, TerminalError};
@@ -12,19 +12,28 @@ impl TerminalAdapter {
     pub fn new() -> Self {
         Self {}
     }
-}
 
-impl TerminalPort for TerminalAdapter {
-    fn open_pane(&self, direction: Direction, size: i32) -> Result<String> {
-        info!("Open wezterm panel: {}", direction.to_string());
-        let pane_size = size.to_string();
-        let args = match direction {
-            Direction::Right => {
-                vec!["cli", "split-pane", "--horizontal", "--percent", &pane_size]
-            }
-            Direction::Down => vec!["cli", "split-pane", "--percent", &pane_size],
-        };
+    fn try_existing_pane(&self, direction: Direction) -> Result<String> {
+        Command::new("wezterm")
+            .args(&["cli", "get-pane-direction", &direction.to_string()])
+            .output()
+            .map_err(|e| anyhow!(e.to_string()))
+            .and_then(|output| {
+                if output.stdout.len() > 0 {
+                    String::from_utf8(output.stdout).map_err(|e| anyhow!(e.to_string()))
+                } else {
+                    Err(anyhow!("No output"))
+                }
+            })
+            .map_err(|e| anyhow!(e.to_string()))
+            .map(|pane_id| pane_id.trim().to_string())
+    }
 
+    fn spawn_new_pane(
+        &self,
+        direction: Direction,
+        args: Vec<&str>,
+    ) -> std::result::Result<String, anyhow::Error> {
         let output = Command::new("wezterm")
             .args(args)
             .output()
@@ -41,6 +50,26 @@ impl TerminalPort for TerminalAdapter {
             Err(TerminalError::OpenPane(format!("There is no pane {direction}")).into())
         } else {
             Ok(pane_id.to_string())
+        }
+    }
+}
+
+impl TerminalPort for TerminalAdapter {
+    fn open_pane(&self, direction: Direction, size: i32) -> Result<String> {
+        info!("Open wezterm panel: {}", direction.to_string());
+        let pane_size = size.to_string();
+        let args = match direction {
+            Direction::Right => {
+                vec!["cli", "split-pane", "--horizontal", "--percent", &pane_size]
+            }
+            Direction::Down => vec!["cli", "split-pane", "--percent", &pane_size],
+        };
+
+        let existing_pane = self.try_existing_pane(direction);
+        if existing_pane.is_ok() {
+            existing_pane
+        } else {
+            self.spawn_new_pane(direction, args)
         }
     }
 
